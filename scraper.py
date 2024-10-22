9b6d6389d86eef7b852a3cca4ad01774fda52cfe
Maissack <franckzhangpro@gmail.com>
2024-10-22 16:09:35 +0200

    :hammer: STORE infos in csv files

--- scraper.py
+++ scraper.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import csv
 
 url = "https://books.toscrape.com/"
-
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -11,24 +11,27 @@ books = []
 
 # Extraction des infos de chaque livre
 for book in soup.find_all('article', class_='product_pod'):
-    title = book.h3.a['title']
-    price = book.find('p', class_='price_color').text
-    availability = book.find('p', class_='instock availability').text.strip()
+    title = book.h3.a['title'].strip()  
+    price = book.find('p', class_='price_color').text.strip() 
+    availability = book.find('p', class_='instock availability').text.strip() 
     link = book.h3.a['href']
-    
-    # Urljoin pour avoir un lien complet
     full_link = urljoin(url, link)
     
     books.append({
-        'title': title,
-        'price': price,
-        'availability': availability,
-        'url': full_link
+        'Title': title,             
+        'Price': price,
+        'Availability': availability,
+        'URL': full_link
     })
 
-# Aesthetics
-print(f"{'Title':<40} {'Price':<10} {'Availability':<15} {'URL'}")
-print("="*100)
+csv_file = "books.csv"
+headers = ['Title', 'Price', 'Availability', 'URL'] 
 
-for book in books:
-    print(f"{book['title']:<40} {book['price']:<10} {book['availability']:<15} {book['url']}")
+with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.DictWriter(file, fieldnames=headers)
+    writer.writeheader()
+    
+    for book in books:
+        writer.writerow(book)
+
+print(f"Les informations des {len(books)} livres ont été stockées dans le fichier '{csv_file}'.")
