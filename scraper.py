2f17f586c0fb6bfa83504eba574058d3c17ed14d
Maissack <franckzhangpro@gmail.com>
2024-10-23 09:12:42 +0200

    :wrench: EXTRACT infos from site

--- scraper.py
+++ scraper.py
@@ -3,35 +3,31 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import csv
 
-url = "https://books.toscrape.com/"
-response = requests.get(url)
+book_url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+response = requests.get(book_url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-books = []
 
-# Extraction des infos de chaque livre
-for book in soup.find_all('article', class_='product_pod'):
-    title = book.h3.a['title'].strip()  
-    price = book.find('p', class_='price_color').text.strip() 
-    availability = book.find('p', class_='instock availability').text.strip() 
-    link = book.h3.a['href']
-    full_link = urljoin(url, link)
-    
-    books.append({
-        'Title': title,             
-        'Price': price,
-        'Availability': availability,
-        'URL': full_link
-    })
+# Extraire les infos du livre
+book_data = {
+    'product_page_url': book_url,
+    'universal_product_code (upc)': soup.find('th', text='UPC').find_next_sibling('td').text,
+    'title': soup.find('h1').text,
+    'price_including_tax': soup.find('th', text='Price (incl. tax)').find_next_sibling('td').text,
+    'price_excluding_tax': soup.find('th', text='Price (excl. tax)').find_next_sibling('td').text,
+    'number_available': soup.find('th', text='Availability').find_next_sibling('td').text,
+    'product_description': soup.find('meta', attrs={'name': 'description'})['content'].strip(),
+    'category': soup.find('ul', class_='breadcrumb').find_all('a')[-1].text,
+    'review_rating': soup.find('p', class_='star-rating')['class'][1],
+    'image_url': urljoin(book_url, soup.find('img')['src'])
+}
 
-csv_file = "books.csv"
-headers = ['Title', 'Price', 'Availability', 'URL'] 
+csv_file = "book_data.csv"
+headers = list(book_data.keys())
 
 with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
     writer = csv.DictWriter(file, fieldnames=headers)
     writer.writeheader()
-    
-    for book in books:
-        writer.writerow(book)
+    writer.writerow(book_data)
 
-print(f"Les informations des {len(books)} livres ont été stockées dans le fichier '{csv_file}'.")
+print(f"Les informations du livre ont été stockées dans le fichier '{csv_file}'.")
